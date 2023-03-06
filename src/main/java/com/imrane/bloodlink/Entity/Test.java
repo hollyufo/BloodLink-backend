090a8823e00ad0daaa44435454f76a81d7076811
imrane chaibi <54860557+hollyufo@users.noreply.github.com>
2023-03-06 15:06:48 +0100

    cretaed test entity

---
+++ src/main/java/com/imrane/bloodlink/Entity/Test.java
@@ -0,0 +1,29 @@
+package com.imrane.bloodlink.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Test {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private String result;
+}
