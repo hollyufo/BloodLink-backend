b8a3343b9724e535d8f8728915f9c61781aa96a0
imrane chaibi <54860557+hollyufo@users.noreply.github.com>
2023-03-06 15:01:19 +0100

    created donation entity

---
+++ src/main/java/com/imrane/bloodlink/Entity/Donation.java
@@ -0,0 +1,24 @@
+package com.imrane.bloodlink.Entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Donation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
