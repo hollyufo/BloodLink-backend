3c6b0ad9d08637aa02272fa17d427aa50631d771
imrane chaibi <54860557+hollyufo@users.noreply.github.com>
2023-03-06 14:57:48 +0100

    created hospitals entity

---
+++ src/main/java/com/imrane/bloodlink/Entity/Hospital.java
@@ -0,0 +1,28 @@
+package com.imrane.bloodlink.Entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Hospital {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String address;
+    private String phone;
+    private String email;
+    private String image;
+    private String map;
+    // city
+    @ManyToOne
+    private City city;
+}
