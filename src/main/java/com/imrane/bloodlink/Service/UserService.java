be91fabbd24a8aad4dcd9212c1d471e7e9a6588f
imrane chaibi <54860557+hollyufo@users.noreply.github.com>
2023-03-18 12:21:31 +0100

    Create UserService.java

---
+++ src/main/java/com/imrane/bloodlink/Service/UserService.java
@@ -0,0 +1,16 @@
+package com.imrane.bloodlink.Service;
+
+
+import com.imrane.bloodlink.Repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+
+
+}
